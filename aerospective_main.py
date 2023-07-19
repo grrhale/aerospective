2dbf4f33094741b26ed5b088c4b35e04852918b2
rhale <raymond.hale0@gmail.com>
2023-07-19 19:25:12 -0400

    young commit - encoding api url, first functions established

---
+++ aerospective_main.py
@@ -0,0 +1,22 @@
+#  AEROSPECTIVE
+#  Data pulled from:
+#  EPA government AirNow API: https://www.airnow.gov/
+#  and from the National Weather Service API: https://api.weather.gov/
+
+import pandas as pd
+import seaborn as sb
+import api_module as am
+
+# Ask user 
+user_zip = input(f'Please enter your zipcode (#####): ')
+
+latlong = am.get_location(user_zip)
+print(am.zip_square(latlong[0], latlong[1]))
+
+test_f_input = am.zip_square(latlong[0], latlong[1])
+test_date_print = am.AirNow_pull(test_f_input[0], 
+	test_f_input[1], 
+	test_f_input[2], 
+	test_f_input[3])
+
+print(test_date_print)
